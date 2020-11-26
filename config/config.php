816e930e51aabdc774335e2bfd63ed35944891f7
Eng.Diogo <dev.diogovdcpa@gmail.com>
2020-11-26 14:37:28 -0300

    up code url

--- config/config.php
+++ config/config.php
@@ -10,8 +10,5 @@ define('CONTROLLER_PADRAO', 'home');
 define('METODO_PADRAO', 'index');
 define('NAMESPACE_CONTROLLER', 'app\\controllers\\');
 
-define('URL_BASE', $PRODUÇAO);
-define('ASSETS', $PRODUÇAO."assets/");
-
-
-$PRODUÇAO = "https://controle-de-estoque-sinapse.herokuapp.com/";
+define('URL_BASE', 'https://controle-de-estoque-sinapse.herokuapp.com/');
+define('ASSETS', 'https://controle-de-estoque-sinapse.herokuapp.com/assets');
