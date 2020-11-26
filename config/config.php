741658f634257f956d0290e100e9e7695058491a
Eng.Diogo <dev.diogovdcpa@gmail.com>
2020-11-26 14:35:26 -0300

    colocando em porduçao

--- config/config.php
+++ config/config.php
@@ -10,10 +10,8 @@ define('CONTROLLER_PADRAO', 'home');
 define('METODO_PADRAO', 'index');
 define('NAMESPACE_CONTROLLER', 'app\\controllers\\');
 
-define('URL_BASE', 'http://localhost/sinapse-controle-de-estoque/');
-define('ASSETS', 'http://localhost/sinapse-controle-de-estoque/assets/');
+define('URL_BASE', $PRODUÇAO);
+define('ASSETS', $PRODUÇAO."assets/");
 
 
 $PRODUÇAO = "https://controle-de-estoque-sinapse.herokuapp.com/";
-define('URL_PROD',$PRODUÇAO);
-define('ASSETS_PROD',$PRODUÇAO."assets/");
