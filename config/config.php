37b5417f6f29f2fb9160eeba80854c104e0e0716
Eng.Diogo <dev.diogovdcpa@gmail.com>
2020-11-26 14:41:42 -0300

    assets

--- config/config.php
+++ config/config.php
@@ -11,4 +11,4 @@ define('METODO_PADRAO', 'index');
 define('NAMESPACE_CONTROLLER', 'app\\controllers\\');
 
 define('URL_BASE', 'https://controle-de-estoque-sinapse.herokuapp.com/');
-define('ASSETS', 'https://controle-de-estoque-sinapse.herokuapp.com/assets');
+define('ASSETS', 'https://controle-de-estoque-sinapse.herokuapp.com/assets/');
